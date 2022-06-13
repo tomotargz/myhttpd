3e9e492ffbc33a2f3e965c601bbaeafa95bc8399
tomotargz <nakamuratomota@gmail.com>
2022-06-13 21:02:02 +0900

    Allow cat to read stdin

--- section5/cat.c
+++ section5/cat.c
@@ -5,36 +5,35 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-static void do_cat(const char* path);
+static void do_cat(int fd, const char* s);
 static void die(const char* s);
 
 int main(int argc, char* argv[]) {
   int i;
   if (argc < 2) {
-    fprintf(stderr, "%s: file name not given\n", argv[0]);
+    do_cat(STDIN_FILENO, "stdin");
     exit(1);
   }
   for (i = 1; i < argc; i++) {
-    do_cat(argv[i]);
+    int fd = open(argv[i], O_RDONLY);
+    if (fd < 0) die(argv[i]);
+    do_cat(fd, argv[i]);
   }
   exit(0);
 }
 
 #define BUFFER_SIZE 2048
-static void do_cat(const char* path) {
-  int fd;
+static void do_cat(int fd, const char* s) {
   unsigned char buf[BUFFER_SIZE];
   int n;
 
-  fd = open(path, O_RDONLY);
-  if (fd < 0) die(path);
   while (1) {
     n = read(fd, buf, sizeof buf);
-    if (n < 0) die(path);
+    if (n < 0) die(s);
     if (n == 0) break;
-    if (write(STDOUT_FILENO, buf, n) < 0) die(path);
+    if (write(STDOUT_FILENO, buf, n) < 0) die(s);
   }
-  if (close(fd) < 0) die(path);
+  if (close(fd) < 0) die(s);
 }
 
 static void die(const char* s) {
