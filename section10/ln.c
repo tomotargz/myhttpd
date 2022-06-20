175b1bbd1e5ebe1dfd53ccba0d795d2edcba08d1
tomotargz <nakamuratomota@gmail.com>
2022-06-20 21:46:39 +0900

    Add ln command

---
+++ section10/ln.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s SRC DEST\n", argv[0]);
+    exit(1);
+  }
+  if (link(argv[1], argv[2]) == -1) {
+    perror(argv[1]);
+    exit(1);
+  }
+  exit(0);
+}
