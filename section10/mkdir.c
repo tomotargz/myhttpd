e4c47697bf7257c44a84b96fe32038e83d7001a9
tomotargz <nakamuratomota@gmail.com>
2022-06-20 21:27:40 +0900

    Add mkdir command

---
+++ section10/mkdir.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s: no arguments\n", argv[0]);
+  }
+  for (int i = 1; i < argc; ++i) {
+    if (mkdir(argv[i], 0777) < 0) {
+      perror(argv[i]);
+      exit(1);
+    }
+  }
+  exit(0);
+}
