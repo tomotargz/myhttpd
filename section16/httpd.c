285165639a99def6be10fd2b3f4cfcbe63c50a55
tomotargz <nakamuratomota@gmail.com>
2022-07-21 21:46:02 +0900

    Add respond_to function

--- section16/httpd.c
+++ section16/httpd.c
@@ -1,11 +1,14 @@
 #define _GNU_SOURCE
 #include <errno.h>
+#include <fcntl.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
 struct HTTPHeaderField {
   char* name;
@@ -43,6 +46,14 @@ static char* lookup_header_field_value(struct HTTPRequest* req, char* name);
 static struct FileInfo* get_fileinfo(char* docroot, char* urlpath);
 static void free_fileinfo(struct FileInfo* f);
 static char* build_fspath(char* docroot, char* urlpath);
+static void respond_to(struct HTTPRequest* req, FILE* out, char* docroot);
+static void do_file_response(struct HTTPRequest* req, FILE* out, char* docroot);
+static void method_not_allowed(struct HTTPRequest* req, FILE* out);
+static void not_implemented(struct HTTPRequest* req, FILE* out);
+static void not_found(struct HTTPRequest* req, FILE* out);
+static void output_common_header_fields(struct HTTPRequest* req, FILE* out,
+                                        char* status);
+static char* guess_content_type(struct FileInfo* f);
 static void upcase(char* str);
 
 int main(int argc, char* argv[]) {
@@ -90,12 +101,12 @@ static void signal_exit(int sig) {
 
 static void service(FILE* in, FILE* out, char* docroot) {
   struct HTTPRequest* req = read_request(in);
-  // respond_to(req, out, docroot);
+  respond_to(req, out, docroot);
   free_request(req);
 }
 
 static void free_request(struct HTTPRequest* req) {
-  for (struct HTTPHeaderField* h; h;) {
+  for (struct HTTPHeaderField* h = req->header; h;) {
     struct HTTPHeaderField* next = h->next;
     free(h->name);
     free(h->value);
@@ -225,4 +236,82 @@ static char* build_fspath(char* docroot, char* urlpath) {
   char* path = xmalloc(strlen(docroot) + strlen(urlpath) + 2);
   sprintf(path, "%s/%s", docroot, urlpath);
   return path;
+}
+
+static void respond_to(struct HTTPRequest* req, FILE* out, char* docroot) {
+  if (!strcmp(req->method, "GET")) {
+    do_file_response(req, out, docroot);
+  } else if (!strcmp(req->method, "HEAD")) {
+    do_file_response(req, out, docroot);
+  } else if (!strcmp(req->method, "POST")) {
+    method_not_allowed(req, out);
+  } else {
+    not_implemented(req, out);
+  }
+}
+
+static const size_t BLOCK_BUF_SIZE = 4096;
+
+static void do_file_response(struct HTTPRequest* req, FILE* out,
+                             char* docroot) {
+  struct FileInfo* info = get_fileinfo(docroot, req->path);
+  if (!info->ok) {
+    free_fileinfo(info);
+    not_found(req, out);
+    return;
+  }
+  output_common_header_fields(req, out, "200 OK");
+  fprintf(out, "Content-Length: %ld\r\n", info->size);
+  fprintf(out, "Content-Type: %s\r\n", guess_content_type(info));
+  fprintf(out, "\r\n");
+  if (strcmp(req->method, "HEAD")) {
+    int fd = open(info->path, O_RDONLY);
+    if (fd < 0) log_exit("failed to open %s: %s", info->path, strerror(errno));
+    while (1) {
+      char buf[BLOCK_BUF_SIZE];
+      ssize_t n = read(fd, buf, BLOCK_BUF_SIZE);
+      if (n < 0) log_exit("failed to read %s: %s", info->path, strerror(errno));
+      if (n == 0) break;
+      if (fwrite(buf, n, 1, out) < 1) {
+        log_exit("failed to write to socket: %s", strerror(errno));
+      }
+    }
+    close(fd);
+  }
+  fflush(out);
+  free_fileinfo(info);
+}
+
+static void method_not_allowed(struct HTTPRequest* req, FILE* out) {
+  output_common_header_fields(req, out, "405 Method Not Allowed");
+}
+
+static void not_implemented(struct HTTPRequest* req, FILE* out) {
+  output_common_header_fields(req, out, "400 Bad Request");
+}
+
+static void not_found(struct HTTPRequest* req, FILE* out) {
+  output_common_header_fields(req, out, "404 Not Found");
+}
+
+static const size_t TIME_BUF_SIZE = 1024;
+static const int HTTP_MINOR_VERSION = 0;
+static const char* SERVER_NAME = "myHTTP";
+static const char* SERVER_VERSION = "1.0";
+
+static void output_common_header_fields(struct HTTPRequest* req, FILE* out,
+                                        char* status) {
+  time_t t = time(NULL);
+  struct tm* tm = gmtime(&t);
+  if (!tm) log_exit("gmtime() failed: %s", strerror(errno));
+  char buf[TIME_BUF_SIZE];
+  strftime(buf, sizeof buf, "%a, %d %b %Y %H:%M:%S GMT", tm);
+  fprintf(out, "HTTP/1.%d %s\r\n", HTTP_MINOR_VERSION, status);
+  fprintf(out, "Date: %s\r\n", buf);
+  fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
+  fprintf(out, "Connection: close\r\n");
+}
+
+static char* guess_content_type(struct FileInfo* f) {
+  return "text/plain";
 }
